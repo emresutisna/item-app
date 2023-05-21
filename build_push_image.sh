26f25e523dad7f173656d912fe205e98e039a828
Nanang Sutisna <emre@192.168.1.6>
2023-05-21 09:25:55 +0700

    initial commit

---
+++ build_push_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Membuat Docker image dari Dockerfile, dengan nama image item-app, dan memiliki tag v1
+docker build -t item-app:v1 .
+
+# Melihat daftar image di lokal.
+docker images
+
+# Mengubah nama image agar sesuai dengan format GitHub Packages
+docker tag item-app:v1 emresutisna/item-app:v1
+
+# Login ke GitHub Packages
+echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u emresutisna --password-stdin
+
+# Mengunggah image ke GitHub Packages
+docker push emresutisna/item-app:v1
